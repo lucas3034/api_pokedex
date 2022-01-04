19e057df63e0c423e8102fad148d5f698a840b88
Lucas Marinho <lucasmarinhho@hotmail.com>
2022-01-04 17:34:50 -0300

    Pokedex Att - New Function(wip)

--- functions/functions.go
+++ functions/functions.go
@@ -1,13 +1,110 @@
 package functions
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
-func Mensagem(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Bem-vindos a Pokedéx")
+type Pokemon struct {
+	Id   int
+	Nome string
+	Tipo string
 }
 
+var Pokemons []Pokemon = []Pokemon{
+	{
+		Id:   1,
+		Nome: "Bulbassauro",
+		Tipo: "Grama/Veneno",
+	},
+	{
+		Id:   2,
+		Nome: "Ivyssauro",
+		Tipo: "Grama/Veneno",
+	},
+	{
+		Id:   3,
+		Nome: "Venossauro",
+		Tipo: "Grama/Veneno",
+	},
+	{
+		Id:   4,
+		Nome: "Charmander",
+		Tipo: "Fogo",
+	},
+	{
+		Id:   5,
+		Nome: "Charmeleon",
+		Tipo: "Fogo",
+	},
+	{
+		Id:   6,
+		Nome: "Charizard",
+		Tipo: "Fogo/Voador",
+	},
+	{
+		Id:   7,
+		Nome: "Squirtle",
+		Tipo: "Água",
+	},
+	{
+		Id:   8,
+		Nome: "Wartortle",
+		Tipo: "Água",
+	},
+	{
+		Id:   9,
+		Nome: "Blastoise",
+		Tipo: "Água",
+	},
+}
 
-//Não estou conseguindo chamar essa função no main
+func MostrarPokemons(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+	encoder.Encode(Pokemons)
+}
+
+func CadastrarPokemons(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	body, _ := ioutil.ReadAll(r.Body)
+	var novoPokemon Pokemon
+	json.Unmarshal(body, &novoPokemon)
+	novoPokemon.Id = len(Pokemons) + 1
+	novoPokemon.Nome = "Novo"
+	novoPokemon.Tipo = "Tipo"
+	Pokemons = append(Pokemons, novoPokemon)
+
+	encoder := json.NewEncoder(w)
+	encoder.Encode(Pokemons)
+}
+
+//func BuscarPokemons(w http.ResponseWriter, r *http.Request){
+//	//w.Header().Set("Content-Type", "application/json")
+//	fmt.Println(r.URL.Path)
+//	valor := strings.Split(r.URL.Path, "/")
+//
+//	if len(valor) > 9 {
+//		w.WriteHeader(http.StatusNotFound)
+//		return
+//	}
+//
+//	id, _ := strconv.Atoi(valor[2])
+//
+//	for _, Pokemon := range Pokemons {
+//		if Pokemons.Id == id {
+//			json.NewEncoder(w).Encode(Pokemon)
+//			return
+//		}
+//	}
+//
+//
+//}
+
+func Mensagem(w http.ResponseWriter) {
+	fmt.Fprintf(w, "Bem-vindos a Pokedéx")
+}
--- main.go
+++ main.go
@@ -1,13 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-
 	"github.com/lucas3034/api_pokedex/functions"
-
-	//"io/ioutil"
 	"net/http"
 )
 
@@ -17,95 +12,20 @@ func main() {
 	http.ListenAndServe(":7000", nil)
 }
 
-type Pokemon struct {
-	Id   int
-	Nome string
-	Tipo string
-}
-
-var Pokemons []Pokemon = []Pokemon{
-	{
-		Id:   1,
-		Nome: "Bulbassauro",
-		Tipo: "Grama/Veneno",
-	},
-	{
-		Id:   2,
-		Nome: "Ivyssauro",
-		Tipo: "Grama/Veneno",
-	},
-	{
-		Id:   3,
-		Nome: "Venossauro",
-		Tipo: "Grama/Veneno",
-	},
-	{
-		Id:   4,
-		Nome: "Charmander",
-		Tipo: "Fogo",
-	},
-	{
-		Id:   5,
-		Nome: "Charmeleon",
-		Tipo: "Fogo",
-	},
-	{
-		Id:   6,
-		Nome: "Charizard",
-		Tipo: "Fogo/Voador",
-	},
-	{
-		Id:   7,
-		Nome: "Squirtle",
-		Tipo: "Água",
-	},
-	{
-		Id:   8,
-		Nome: "Wartortle",
-		Tipo: "Água",
-	},
-	{
-		Id:   9,
-		Nome: "Blastoise",
-		Tipo: "Água",
-	},
-}
-
 func inicio(w http.ResponseWriter, r *http.Request) {
-	functions.Mensagem(w, r)
+	functions.Mensagem(w)
 }
-
-func mostrarPokemons(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	encoder := json.NewEncoder(w)
-	encoder.Encode(Pokemons)
-}
-
- func cadastrarPokemons(w http.ResponseWriter, r *http.Request) {
- 	w.Header().Set("Content-Type", "application/json")
- 	w.WriteHeader(http.StatusCreated)
-
- 	body, _ := ioutil.ReadAll(r.Body)
- 	var novoPokemon Pokemon
- 	json.Unmarshal(body, &novoPokemon)
- 	novoPokemon.Id = len(Pokemons) + 1
- 	novoPokemon.Nome = "Novo"
- 	novoPokemon.Tipo = "Tipo"
- 	Pokemons = append(Pokemons, novoPokemon)
-
- 	encoder := json.NewEncoder(w)
- 	encoder.Encode(Pokemons)
- }
 
 func rotas() {
 	http.HandleFunc("/", inicio)
 	http.HandleFunc("/pokemons", rotasPokemons)
+	//http.HandleFunc("/pokemons/", functions.BuscarPokemons)
 }
 
 func rotasPokemons(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
-		mostrarPokemons(w, r)
+		functions.MostrarPokemons(w)
 	} else if r.Method == "POST" {
-		cadastrarPokemons(w, r)
+		functions.CadastrarPokemons(w, r)
 	}
 }
