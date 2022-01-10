5889dd458efa38bcb06934b0728aa74a7214d37d
Lucas Marinho <lucasmarinhho@hotmail.com>
2022-01-10 15:39:06 -0300

    Pokedex Att - Code improvement

--- main.go
+++ main.go
@@ -5,10 +5,11 @@ import (
 	"net/http"
 
 	"github.com/lucas3034/api_pokedex/functions"
+	"github.com/lucas3034/api_pokedex/routes"
 )
 
 func main() {
-	routes()
+	localRoutes()
 	fmt.Println("Server on port: 7000")
 	http.ListenAndServe(":7000", nil)
 }
@@ -17,34 +18,9 @@ func Start(w http.ResponseWriter, r *http.Request) {
 	functions.Message(w)
 }
 
-func routes() {
+func localRoutes() {
 	http.HandleFunc("/", Start)
-	http.HandleFunc("/pokemons", RoutesPokemons)
-	http.HandleFunc("/pokemons/", ListsPokemons)
+	http.HandleFunc("/pokemons", routes.RoutesPokemons)
+	http.HandleFunc("/pokemons/", routes.ListsPokemons)
 }
 
-func RoutesPokemons(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusMultipleChoices)
-	if r.Method == "GET" {
-		functions.ShowPokemons(w)
-	} else if r.Method == "POST" {
-		functions.RegisterPokemons(w, r)
-	} else {
-		fmt.Fprintf(w, "The method entered is incorrect")
-	}
-}
-
-func ListsPokemons(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusMultipleChoices)
-	if r.Method == "GET" {
-		functions.SearchPokemons(w, r)
-	} else if r.Method == "DELETE" {
-		functions.DeletePokemons(w, r)
-	} else if r.Method == "PUT" {
-		functions.EditPokemons(w, r)
-	} else {
-		fmt.Fprintf(w, "The method entered is incorrect")
-	}
-}
--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,33 @@
 package routes
+
+import (
+	"fmt"
+	"github.com/lucas3034/api_pokedex/functions"
+	"net/http"
+)
+
+func RoutesPokemons(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusMultipleChoices)
+	if r.Method == "GET" {
+		functions.ShowPokemons(w)
+	} else if r.Method == "POST" {
+		functions.RegisterPokemons(w, r)
+	} else {
+		fmt.Fprintf(w, "The method entered is incorrect")
+	}
+}
+
+func ListsPokemons(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusMultipleChoices)
+	if r.Method == "GET" {
+		functions.SearchPokemons(w, r)
+	} else if r.Method == "DELETE" {
+		functions.DeletePokemons(w, r)
+	} else if r.Method == "PUT" {
+		functions.EditPokemons(w, r)
+	} else {
+		fmt.Fprintf(w, "The method entered is incorrect")
+	}
+}
